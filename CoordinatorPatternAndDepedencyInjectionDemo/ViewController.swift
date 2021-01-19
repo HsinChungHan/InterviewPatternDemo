f997d90cbfed5019154f8f123eedf62ac967746c
HsinChungHan <hooy123456@gapp.nthu.edu.tw>
2021-01-20 00:57:34 +0800

    Initial Commit

---
+++ CoordinatorPatternAndDepedencyInjectionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorPatternAndDepedencyInjectionDemo
+//
+//  Created by Chung Han Hsin on 2021/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
